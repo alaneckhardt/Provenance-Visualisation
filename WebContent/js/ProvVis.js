6d13ec6a4e9667f7e1120b9a4db4e1ae7bff9f94
alaneckhardt <a.e@centrum.cz>
2012-08-09 15:25:13 +0100

    Added javascriptDoc.

--- WebContent/js/ProvVis.js
+++ WebContent/js/ProvVis.js
@@ -3,59 +3,48 @@
 	var selected1 = null, selected2 = null;
 	var selectedEdge;
 	Math.seedrandom(seed);
-	//Add handling of the ctrl key
-		ctrlKey = false;
-		shiftKey = false;
-		metaKey = false;
-		$(document).bind('keyup keydown', function(e){ctrlKey = e.ctrlKey;} );
-		$(document).bind('keyup keydown', function(e){shiftKey = e.shiftKey;} );
-		$(document).bind('keyup keydown', function(e){metaKey = e.metaKey;} );
-	//var server = 'http://mrt.esc.abdn.ac.uk:8080/ProvenanceService/';
-	//var serverVisual = 'http://mrt.esc.abdn.ac.uk:8080/ourspaces/testProvenance/';
-
-		
+	
+/**
+ * Class for provenance visualisation. 
+ * @returns
+ */
 function ProvVis() {
-	/*jsPlumb.reset();
-	jsPlumb.ready(function() {
-		jsPlumb.Defaults.PaintStyle = {
-				lineWidth : 4,
-				strokeStyle : "#aaa"
-			};
-			jsPlumb.Defaults.Endpoint = [ "Dot", {
-				radius : 10
-			}, {
-				isSource : true,
-				isTarget : true
-			} ];
-			jsPlumb.Defaults.MaxConnections = 10;
-			jsPlumb.Defaults.Container = $("#" + jsPlumb.canvas);
-	});*/
+	/**Index of the instance in the array of the parent.*/
 	this.index = -1;
+	/**Array of children instances, in case there is more than one graph on a page.*/
 	this.array = [];
+	/**Class handling the visualisation of graph.*/	
 	this.graph = new ProvVisGraph(this);
+	/**Core function of provvis.*/
 	this.core = new ProvVisCore(this);
+	/**Communication with the Provenance Service at the server-side.*/
 	this.comm = new ProvVisComm(this);
+	/**Layout of the graph.*/
 	this.layout = new ProvVisLayout(this);	
+	/**Functions for editing the provenance.*/
 	this.edit = new ProvVisEdit(this);	
+	/**Class for connecting the nodes.*/
 	this.jsPlumb = jsPlumb.getInstance();
-		this.jsPlumb.reset();
-		this.jsPlumb.Defaults.PaintStyle = {
+	this.jsPlumb.reset();
+	this.jsPlumb.Defaults.PaintStyle = {
 				lineWidth : 4,
 				strokeStyle : "#aaa"
-			};
-		this.jsPlumb.Defaults.Endpoint = [ "Dot", {
+	};
+	this.jsPlumb.Defaults.Endpoint = [ "Dot", {
 				radius : 10
 			}, {
 				isSource : true,
 				isTarget : true
-			} ];
-		this.jsPlumb.Defaults.MaxConnections = 10;
-		this.jsPlumb.Defaults.Container = $("#" + this.jsPlumb.canvas);
+	} ];
+	this.jsPlumb.Defaults.MaxConnections = 10;
+	this.jsPlumb.Defaults.Container = $("#" + this.jsPlumb.canvas);
+	/**Creates an instance of ProvVis and adds it to the array.*/
 	this.getProvVis = function(){
 		var p = new ProvVis();
 		p.index = this.array.length;
 		this.array.push(p);
 		return p;
-	}
+	};
 }
+/**Main instance of visualisation. If there is more graphs on one page, the array in this instance will contain all the ProvVis instances in the array field. */
 provVis = new ProvVis();
